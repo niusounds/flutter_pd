932ff44069d3f529d9fdb57c0ef9086f9560d996
Yuya Matsuo <niu@niusounds.com>
2022-09-25 00:32:13 +0900

    reformat kotlin codes

--- android/src/main/kotlin/com/niusounds/flutter_pd/AssetPathResolver.kt
+++ android/src/main/kotlin/com/niusounds/flutter_pd/AssetPathResolver.kt
@@ -1,27 +1,18 @@
 package com.niusounds.flutter_pd
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.PluginRegistry
 
 /**
  * Resolve asset path from Flutter to actual asset path in apk.
  */
 interface AssetPathResolver {
-  fun resolve(assetPath: String): String
+    fun resolve(assetPath: String): String
 }
 
 fun FlutterPlugin.FlutterPluginBinding.asAssetPathResolver(): AssetPathResolver {
-  return object : AssetPathResolver {
-    override fun resolve(assetPath: String): String {
-      return flutterAssets.getAssetFilePathByName(assetPath)
+    return object : AssetPathResolver {
+        override fun resolve(assetPath: String): String {
+            return flutterAssets.getAssetFilePathByName(assetPath)
+        }
     }
-  }
-}
-
-fun PluginRegistry.Registrar.asAssetPathResolver(): AssetPathResolver {
-  return object : AssetPathResolver {
-    override fun resolve(assetPath: String): String {
-      return lookupKeyForAsset(assetPath)
-    }
-  }
 }
